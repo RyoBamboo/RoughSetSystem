d065d1fb96d364173c5de95b4dc4d82bf6925679
BambooUniv <s175026@center.wakayama-u.ac.jp>
2015-08-05 16:07:36 +0900

    不要なAdminLTEを削除

--- app/views/thesaurus/add.blade.php
+++ app/views/thesaurus/add.blade.php
@@ -1,4 +1,10 @@
 @extends('base')
+
+@section('css')
+    <link rel="stylesheet" type="text/css" href={{asset("/assets/css/uikit/css/components/progress.gradient.css")}}>
+@endsection
+
+
 @section('content')
     <div class="uk-container uk-container-center">
         <div class="uk-grid">
@@ -8,6 +14,15 @@
             <div class="tm-main uk-width-8-10">
                 <h1 class="uk-article-title">Thesauru</h1>
                 <form class="uk-form">
+                    <select>
+                        <option>文字を入力して登録する</option>
+                        <option>CSVファイルを入力して登録する</option>
+                    </select>
+
+                    <div class="tm-upload-button">
+                        ファイルを選択
+                        <input type="file">
+                    </div>
                     <div class="uk-form-file uk-text-primary">
                         text<input type="file">
                     </div>
