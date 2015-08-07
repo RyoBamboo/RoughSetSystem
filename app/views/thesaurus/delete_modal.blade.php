fdee2ef4393e20e9b7130a3d612f2c3481e1e041
BambooUniv <s175026@center.wakayama-u.ac.jp>
2015-08-07 15:46:16 +0900

    不要なファイルの削除

---
+++ app/views/thesaurus/delete_modal.blade.php
@@ -0,0 +1,14 @@
+<div id="my-id" class="uk-modal">
+    <div class="uk-modal-dialog">
+        <div class="uk-modal-header">
+            感性ワードを削除してもよろしいですか
+        </div>
+        <div class="uk-container uk-container-center">
+            一度削除すると復元することはできません
+        </div>
+        <div class="uk-modal-footer uk-text-right">
+            <button type="button" class="uk-button">キャンセル</button>
+            <a href="/thesaurus/delete/{{ $thesaurus->id }}" type="button" class="uk-button uk-button-danger">削除</a>
+        </div>
+    </div>
+</div>
