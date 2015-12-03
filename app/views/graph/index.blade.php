3512ba0dec9c934d08be79835c430159dc1bc0d1
BambooUniv <s175026@center.wakayama-u.a>
2015-12-03 18:03:33 +0900

    単体グラフ画面からサイドバーを削除

--- app/views/graph/index.blade.php
+++ app/views/graph/index.blade.php
@@ -10,9 +10,6 @@
     </div>
     <div class="uk-container uk-container-center">
         <div class="uk-grid">
-            <div class="tm-sidebar uk-width-2-10">
-                @include('graph.sidebar')
-            </div>
             <div class="tm-main uk-width-8-10">
                 <h1 class="uk-article-title">Graph</h1><br>
                 <table class="uk-table">
